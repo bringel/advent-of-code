0f9de7df212861a0002769a81406017c0c6d6406
Brad Ringel <brad.ringel@gmail.com>
2023-12-04 09:52:15 -0500

    part 2 solution

--- 2023/02/solution.rb
+++ 2023/02/solution.rb
@@ -55,7 +55,25 @@ def parse_game_line(line)
 end
 
 # ------
+def get_min_cubes(game:)
+  {
+    red: game[:sets].max { |a,b| a["red"].to_i <=> b["red"].to_i }.fetch("red"),
+    green: game[:sets].max { |a,b| a["green"].to_i <=> b["green"].to_i }.fetch("green"),
+    blue: game[:sets].max { |a,b| a["blue"].to_i <=> b["blue"].to_i }.fetch("blue")
+  }
+end
 
+def power(set:)
+  set.values.map(&:to_i).reduce(:*)
+end
+
+def sum_of_game_powers(input:)
+  games = input.map { |l| parse_game_line(l) }
+  games.map { |g| get_min_cubes(game: g) }.sum { |s| power(set: s) }
+end
 
 puts "Part 1:"
 puts sum_possible_game_ids(input: input, red_count: 12, green_count: 13, blue_count: 14)
+
+puts "Part 2:"
+puts sum_of_game_powers(input: input)
