a8b3d0ca8a31fad45d326c116db07a50065dad9b
Brad Ringel <brad.ringel@gmail.com>
2023-12-04 09:39:36 -0500

    refactor part 1 to make part 2 easier

--- 2023/02/solution.rb
+++ 2023/02/solution.rb
@@ -3,43 +3,59 @@
 input = File.readlines('./input.txt', chomp: true)
 
 def sum_possible_game_ids(input:, red_count:, green_count:, blue_count:)
-  get_possible_games(input: input, red_count: red_count, green_count: green_count, blue_count: blue_count).sum do |game_line|
-    id_matcher = %r{Game (\d+):.*}
-    id_matcher.match(game_line).captures.first.to_i
+  games = input.map { |l| parse_game_line(l) }
+  max_cubes = {
+    red: red_count,
+    green: green_count,
+    blue: blue_count
+  }
+  get_possible_games(games: games, max_cubes: max_cubes).sum do |game|
+    game[:id].to_i
   end
 end
 
-def get_possible_games(input:, red_count:, green_count:, blue_count:)
-  game_matcher = %r{Game \d+: (.*)}
-
-  input.filter do |line|
-    game_input = line.match(game_matcher).captures.first
-
-    game_possible?(game: game_input, red_count: red_count, green_count: green_count, blue_count: blue_count)
+def get_possible_games(games:, max_cubes:)
+  games.filter do |game|
+    game_possible?(game: game, max_cubes: max_cubes)
   end
 end
 
-def game_possible?(game:, red_count:, green_count:, blue_count:)
-  sets = game.split(";")
+def game_possible?(game:, max_cubes:)
+  game[:sets].each do |set|
+    red_over = set["red"] ? set["red"].to_i > max_cubes[:red] : false
+    green_over = set["green"] ? set["green"].to_i > max_cubes[:green] : false
+    blue_over = set["blue"] ? set["blue"].to_i > max_cubes[:blue] : false
 
-  matcher = %r{(?:(?=(?<blue>\d+) blue)|(?=(?<green>\d+) green)|(?=(?<red>\d+) red))}
-
-  sets.each do |set|
-    values = set.split(",")
-    value_matches = values.map { |v| v.match(matcher).named_captures }
-  
-    value_matches.each do |set_match|
-      red_over = set_match["red"] ? set_match["red"].to_i > red_count : false
-      green_over = set_match["green"] ? set_match["green"].to_i > green_count : false
-      blue_over = set_match["blue"] ? set_match["blue"].to_i > blue_count : false
-
-      if red_over || green_over || blue_over
-        return false
-      end
+    if red_over || green_over || blue_over
+      return false
     end
   end
   return true
 end
+
+def parse_game_line(line)
+  game_matcher = %r{Game (\d+): (.*)}
+  match = line.match(game_matcher)
+  id, game_input = match.captures
+
+  set_parser = %r{(?:(?=(?<blue>\d+) blue)|(?=(?<green>\d+) green)|(?=(?<red>\d+) red))}
+
+  parsed_sets = game_input.split(';').map do |set|
+    values = set.split(',')
+    value_matches = values.map { |v| v.match(set_parser).named_captures }
+    value_matches.reduce({}) do |acc, current|
+      acc.merge(current.compact)
+    end
+  end
+
+  {
+    id: id,
+    sets: parsed_sets
+  }
+end
+
+# ------
+
 
 puts "Part 1:"
 puts sum_possible_game_ids(input: input, red_count: 12, green_count: 13, blue_count: 14)
