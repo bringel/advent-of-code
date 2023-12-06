69ca6a3b93f34aa956547e9d553244f457e4108d
Brad Ringel <brad.ringel@gmail.com>
2023-12-06 08:59:49 -0500

    part 2 first try

--- 2023/04/solution.rb
+++ 2023/04/solution.rb
@@ -3,11 +3,12 @@
 input = File.readlines("./input.txt", chomp: true)
 
 def get_cards(input:)
-  input.map do |line|
+  input.map.with_index do |line, index|
     numbers = line[line.index(':') + 1, line.length]
     winning_numbers, card_numbers = numbers.split("|").map { |n| n.split(" ").map(&:to_i) }
 
     {
+      card_number: index + 1,
       winning_numbers: winning_numbers,
       card_numbers: card_numbers
     }
@@ -31,3 +32,32 @@ end
 
 puts "Part 1:"
 puts sum_card_scores(input: input)
+
+#---
+
+def process_scratch_cards(cards:)
+  processed_cards = []
+
+  while cards.length > 0
+    card_number = cards.first[:card_number]
+    cards_to_remove = cards.count { |c| c[:card_number] == card_number }
+    current_cards = cards.shift(cards_to_remove)
+    copies = current_cards.flat_map do |c|
+      matches = c[:winning_numbers] & c[:card_numbers]
+      match_copies = (1..matches.length).map do |index|
+        cards.find { |original_card| original_card[:card_number] == (card_number + index) }
+      end
+    end
+    processed_cards.concat(current_cards)
+    cards.unshift(*copies).sort! { |a,b| a[:card_number] <=> b[:card_number] }
+  end
+
+  processed_cards
+end
+
+def count_processed_cards(input:)
+  process_scratch_cards(cards: get_cards(input: input)).length
+end
+
+puts "Part 2:"
+puts count_processed_cards(input: input)
